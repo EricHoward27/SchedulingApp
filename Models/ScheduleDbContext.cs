16f5eeae03b3b951ea03c40df8647ccad288b35e
EricHoward27 <erichoward2141@gmail.com>
2024-05-16 20:38:09 -0500

    Add project files.

---
+++ Models/ScheduleDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingApp.Models
+{
+	public class ScheduleDbContext : DbContext
+	{
+		public ScheduleDbContext() : base("name=ScheduleDbContext")
+		{
+		}
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<City> Cities { get; set; }
+		public DbSet<Country> Countries { get; set; }
+		public DbSet<Address> Addresses { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Appointment> Appointments { get; set; }
+
+	}
+}
